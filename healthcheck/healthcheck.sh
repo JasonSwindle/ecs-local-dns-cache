6ae8c0947df6f679383a0da7b4c299316feea08d
Jason Swindle <Jason.swindle@gmail.com>
2019-06-14 16:07:07 -0700

    Update failure.

--- healthcheck/healthcheck.sh
+++ healthcheck/healthcheck.sh
@@ -17,7 +17,7 @@ echo 'Starting healthcheck.'
 ## check the endpoint. If there is a failure, increment by one.
 while [ $FAILURES -le $MAX_FAILURES ]; do
   sleep $INTERVAL
-  if curl --connect-timeout 5 --silent localhost:$HEALTHCHECK_PORT/health | grep -i -s -q 'OK'; then
+  if curl --connect-timeout $TIMEOUT --silent localhost:$HEALTHCHECK_PORT/health | grep -i -s -q 'OK'; then
     FAILURES=1
   else
     echo "Health check failure" $FAILURES
